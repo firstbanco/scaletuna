0ca95245fa8878f0bbc85bcfddde8a04c4cb2951
Samuel Kleiner <sam@firstbanco.com>
2015-09-01 11:14:10 +0100

    Rubocop

--- scaletuna.rb
+++ scaletuna.rb
@@ -5,7 +5,7 @@ include Curses
 require "aws-sdk"
 Q = Queue.new
 I = []
-def quit(message = nil,code = 0)
+def quit(message = nil, code = 0)
   close_screen
   $stderr.puts message if message
   exit code
@@ -13,23 +13,23 @@ end
 begin
   C = Aws::AutoScaling::Client.new
 rescue Exception => ex
-  quit( "Error: #{ex}", -1 )
+  quit("Error: #{ex}", -1)
 end
-P = Thread.new do  
+P = Thread.new do
   begin
     loop {
       Q << C
-        .describe_auto_scaling_groups
-        .auto_scaling_groups
-        .select{|a| a.auto_scaling_group_name[/#{ARGV[0]||"."}/]}
+           .describe_auto_scaling_groups
+           .auto_scaling_groups
+           .select { |a| a.auto_scaling_group_name[/#{ARGV[0] || "."}/] }
     }
   rescue Exception => ex
-    quit( "Error: #{ex}", -1 )
+    quit("Error: #{ex}", -1)
   end
 end
 init_screen
 def message(msg)
-  setpos(lines-1,0)
+  setpos(lines - 1, 0)
   clrtoeol
   addstr msg
   I << msg
@@ -37,93 +37,93 @@ def message(msg)
 end
 begin
   crmode
-  selected = [0,2]
+  selected = [0, 2]
   stdscr.keypad = true
   stdscr.timeout = 100
   asc = Q.pop
   updated = Time.now
   loop {
     unless Q.empty?
-      asc = Q.pop 
+      asc = Q.pop
       updated = Time.now
     end
     selection = asc[selected[0]]
-    setpos(0,0)
+    setpos(0, 0)
     clrtoeol
     addstr "min     max     desired current name      updated:#{updated}"
-    asc.each_with_index{|e,i|
+    asc.each_with_index {|e, i|
       clrtoeol
       [e.min_size,
        e.max_size,
        e.desired_capacity,
        e.instances.length,
-       e.auto_scaling_group_name].each_with_index{|v,j|
-        (i==selected.first && j==selected.last) && attron(A_BOLD) || attroff(A_BOLD)
-        (i==selected.first || j==selected.last) && attron(A_REVERSE) || attroff(A_REVERSE)
-        setpos(i+1,j*8)
-        addstr v.to_s[0...cols-j*8]
+       e.auto_scaling_group_name].each_with_index {|v, j|
+        (i == selected.first && j == selected.last) && attron(A_BOLD) || attroff(A_BOLD)
+        (i == selected.first || j == selected.last) && attron(A_REVERSE) || attroff(A_REVERSE)
+        setpos(i + 1, j * 8)
+        addstr v.to_s[0...cols - j * 8]
       }
     }
-    setpos(selected.first+1,selected.last*8)
+    setpos(selected.first + 1, selected.last * 8)
     attroff A_BOLD
     refresh
     change = 0
     begin
       case (ch = getch)
       when KEY_UP
-        selected[0]=[selected[0]-1,0].max
+        selected[0] = [selected[0] - 1, 0].max
       when KEY_DOWN
-        selected[0]=[selected[0]+1,asc.length-1].min
-      when KEY_LEFT      
-        selected[1]=[selected[1]-1,0].max
+        selected[0] = [selected[0] + 1, asc.length - 1].min
+      when KEY_LEFT
+        selected[1] = [selected[1] - 1, 0].max
       when KEY_RIGHT
-        selected[1]=[selected[1]+1,2].min
-      when KEY_ENTER,"\n".ord,"\r".ord,"+"
+        selected[1] = [selected[1] + 1, 2].min
+      when KEY_ENTER, "\n".ord, "\r".ord, "+"
         change = +1
-      when KEY_BACKSPACE,"-"
+      when KEY_BACKSPACE, "-"
         change = -1
       when "q"
-        quit (I << "Exited normally").join("\n")      
+        quit (I << "Exited normally").join("\n")
       else
         message "#{ch} not mapped (+,- to increase or decrease, and move with arrows)" if ch
       end
-      if change!=0
+      if change != 0
         name = selection.auto_scaling_group_name
 
         case selected[1]
-        when 2 # desired_capacity          
-          new = selection.desired_capacity+change
-          message "Setting desired capacity of #{name} to #{new}"          
+        when 2 # desired_capacity
+          new = selection.desired_capacity + change
+          message "Setting desired capacity of #{name} to #{new}"
           C.set_desired_capacity(auto_scaling_group_name:
                                  selection.auto_scaling_group_name,
-                                 desired_capacity:new,
-                                 honor_cooldown:false)
+                                 desired_capacity: new,
+                                 honor_cooldown: false)
           selection.desired_capacity = new
         when 0 # min_size
-          new = selection.min_size+change
+          new = selection.min_size + change
           message "Setting min size of #{name} to #{new}"
           C.update_auto_scaling_group(auto_scaling_group_name:
                                       selection.auto_scaling_group_name,
-                                      min_size:new
-                                      )
+                                      min_size: new
+                                     )
           selection.min_size = new
         when 1 # max_size
-          new = selection.max_size+change
+          new = selection.max_size + change
           message "Setting max size of #{name} to #{new}"
           C.update_auto_scaling_group(auto_scaling_group_name:
                                       selection.auto_scaling_group_name,
-                                      max_size:new
-                                      )
+                                      max_size: new
+                                     )
           selection.max_size = new
         end
       end
     rescue Aws::AutoScaling::Errors::ValidationError => err
-      message "Error: #{err.to_s}"
+      message "Error: #{err}"
     end
   }
   refresh
 rescue Interrupt
-  quit("Ctrl-c",130)
+  quit("Ctrl-c", 130)
 ensure
   close_screen
 end
